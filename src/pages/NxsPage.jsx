10b5ed1ca58069a2e964b2b756f2fd4290a38ae9
elasheras <emperorbfd@gmail.com>
2023-12-21 00:19:44 -0300

    Updated links in nexus page. Added kofi QR asset edits.

--- src/pages/NxsPage.jsx
+++ src/pages/NxsPage.jsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function MainContent(){
 	
     return (
 		<section className="s6-layout-fluid">
 			<section className="row text-center">
-				{/* <!-- here should lie the linking of other websites --> */}
 				<article className="col-12 s6-link-nexus-container">
-					<a rel="noreferrer" href="https://twitter.com/lhs_emi" target="_blank" className="s6-btn-link s6-encapsulator-double">Twitter</a>
-					<a rel="noreferrer" href="https://www.printful.com/es/print-on-demand" target="_blank" className="s6-btn-link s6-encapsulator-double">Printful</a>
-					<a rel="noreferrer" href="https://www.instagram.com/emilasheras/" target="_blank" className="s6-btn-link s6-encapsulator-double">Instagram</a>
-					<a rel="noreferrer" href="https://github.com/emilasheras/" target="_blank" className="s6-btn-link s6-encapsulator-double">GitHub</a>
-					<a rel="noreferrer" href="https://paypal.me/syx6pp?country.x=AR&locale.x=en_US" target="_blank" className="s6-btn-link s6-encapsulator-double">PayPal</a>
-					<a rel="noreferrer" href="" target="_blank" className="s6-btn-link s6-encapsulator-double">Wallet</a>
-					<a rel="noreferrer" href="" target="_blank" className="s6-btn-link s6-encapsulator-double">Portfolio</a>
+					<Link to="https://instagram.com/ttemple_syx6/" target="_blank" className="s6-btn-link s6-encapsulator-double">Instagram</Link>
+					<Link to="https://ko-fi.com/ttsyx6" target="_blank" className="s6-btn-link s6-encapsulator-double">Ko-Fi</Link>
+					<Link to="https://paypal.com/paypalme/syx6pp" target="_blank" className="s6-btn-link s6-encapsulator-double">PayPal</Link>
+					<Link to="https://github.com/emilasheras/" target="_blank" className="s6-btn-link s6-encapsulator-double">GitHub</Link>
+					<Link to="https://twitter.com/lhs_emi" target="_blank" className="s6-btn-link s6-encapsulator-double">Twitter</Link>
 				</article>
 			</section>
 		</section>
-    );
+    ); 
 }
+
+// https://printful.com
+// https://twitter.com/
