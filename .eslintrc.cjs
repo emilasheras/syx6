99be12fbfed02ec55cd530140139718dec9d5e98
elasheras <emperorbfd@gmail.com>
2024-01-10 17:45:38 -0300

    Add environment variables for Firebase configuration and update ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -17,5 +17,6 @@ module.exports = {
       { allowConstantExport: true },
     ],
     // 'react/prop-types': 'off',
+    'no-unused-vars': 'warn', // or 'off' to completely disable
   },
 }
