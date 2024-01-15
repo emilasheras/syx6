589d7deee79d5e05072181f0b33c9130c2be4f3d
emilasheras <emperorbfd@gmail.com>
2024-01-15 15:56:48 -0300

    Add page header to ForceAuthForm component and css component for the form itself. will b refactored later

--- src/components/auth/form/ForceAuthForm.jsx
+++ src/components/auth/form/ForceAuthForm.jsx
@@ -1,5 +1,6 @@
 import { useLocation, Navigate } from "react-router-dom";
 import useEmailAndPasswordAuth from "../../../hooks/useEmailAndPasswordAuth";
+import PageHeader from "../../page-heading/Default";
 
 const ForceAuthForm = () => {
     const { email, setEmail, password, setPassword, signIn, signUp, isGuest } = useEmailAndPasswordAuth();
@@ -18,32 +19,43 @@ const ForceAuthForm = () => {
 
     if (!isGuest) return <Navigate to={redirectAfterAuth} />; // <- Redirects to the saved location or default route
 
-    // If the user is not authentificated, aka a guest, show the sign in form
+
+    const title = "Authenticate";
+    const description = "Please sign in to continue.";
+
+    // If the user is not authentificated, aka its a guest, show the sign in form
     return (
-        <form>
-            <div>
-                <label>Email</label>
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    autoComplete="email"
-                />
+        <div>
+            <PageHeader title={title} extraMessage={description} />            
+            <div className="s6-form-container">
+                <form className="s6-form">
+                    <div>
+                        {/* <label>Email</label> */}
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                            autoComplete="email"
+                            placeholder="Email"
+                        />
+                    </div>
+                    <div>
+                        {/* <label>Password</label> */}
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            autoComplete="current-password"
+                            placeholder="Password"
+                        />
+                    </div>
+                    <button className="s6-form-button" type="button" onClick={handleSignUpClick}>Sign Up</button>
+                    <button className="s6-form-button" type="button" onClick={handleSignInClick}>Sign In</button>
+                </form>
             </div>
-            <div>
-                <label>Password</label>
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    autoComplete="current-password"
-                />
-            </div>
-            <button type="button" onClick={handleSignUpClick}>Sign Up</button>
-            <button type="button" onClick={handleSignInClick}>Sign In</button>
-        </form>
+        </div>
     );
 };
 
