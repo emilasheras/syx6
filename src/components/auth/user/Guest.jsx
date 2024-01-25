1c54331887413984e637877214986c08cb2641af
elasheras <emperorbfd@gmail.com>
2024-01-24 22:15:17 -0300

    Added links and button for login in Guest component and remove commented code in SignOutButton component

--- src/components/auth/user/Guest.jsx
+++ src/components/auth/user/Guest.jsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
 import SyxParagraph from '../../paragraph/SyxParagraph'
 
 const Guest = () => {
     const defaultMessage = 'Guest User'
     return (
-        <SyxParagraph text={defaultMessage}/>
-    )
+        <>
+            <SyxParagraph text={defaultMessage}/>
+            <Link to='/auth/login'><button>Log In</button></Link>
+        </>
+    );
 }
 
 export default Guest
--- src/components/auth/user/SignOutButton.jsx
+++ src/components/auth/user/SignOutButton.jsx
@@ -1,11 +1,6 @@
 import useEmailAndPasswordAuth from "../../../hooks/useEmailAndPasswordAuth";
-
-
 const SignOutButton = () => {
     const { signOut } = useEmailAndPasswordAuth();
-
-    // {error && <p>{error}</p>}
 	return <button onClick={() => signOut()}>Sign Out</button>
 };
-
 export default SignOutButton;
